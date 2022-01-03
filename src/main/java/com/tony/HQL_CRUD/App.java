1ef11728d5113110535379e5c4600002da2cc7ee
SIVAJI KADAPALA <83819803+sivaji-Kadapala@users.noreply.github.com>
2022-01-03 22:16:42 +0530

    CRUD

---
+++ src/main/java/com/tony/HQL_CRUD/App.java
@@ -0,0 +1,13 @@
+package com.tony.HQL_CRUD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
